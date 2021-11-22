d81eff51f88de7cc5db3d4615861943fe059362b
Brandon Bremer <beb8nj@virginia.edu>
2021-11-22 15:53:31 -0500

    SQL Changes

--- author/app.js
+++ author/app.js
@@ -4,11 +4,33 @@ var path = require("path");
 const fetch = require("node-fetch");
 var admin = require("firebase-admin");
 var serviceAccount = require("./accountKey2.json");
+const mysql = require("mysql");
+const dbConfig = require("./db.config.js");
+const sql = require("./db.js");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
 const port = 3001;
+
+findById = (id, result) => {
+  sql.query(`SELECT title FROM book HAVING title = "${id}" `, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      //result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found book: ", res[0]);
+      //result(null, res[0]);
+      return;
+    }
+
+    // not found Tutorial with the id
+    //result({ kind: "not_found" }, null);
+  });
+};
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -20,6 +42,7 @@ app.listen(port, () => {
 
 app.get("/search/:query", function (req, res) {
   const title = req.params.query;
+  findById(title);
   fetch(
     `https://www.googleapis.com/books/v1/volumes?q=${title}+&key=AIzaSyB6vrb-b0HwJDZTHPyHd_skMT41qBuVI34`
   )
